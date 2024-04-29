28a20d787957871afd1b280fbca833fd24836c67
Viktor234213 <166949837+Viktor234213@users.noreply.github.com>
2024-04-29 04:44:34 +0200

    Latest one!

---
+++ newestclasses.rb
@@ -0,0 +1,177 @@
+# classes.rb
+
+
+class Output
+  attr_accessor :value
+  
+  def initialize(value)
+    @value = value
+  end
+
+  def eval
+    puts @value.eval
+  end
+end
+
+
+class Input
+  attr_accessor :value
+
+  def initialize(value)
+    @value = value
+  end
+
+  def eval
+    input = gets.chomp
+    Scope.set_variable(@value, input)
+  end
+end
+
+class Assignment
+  attr_accessor :variable, :value
+
+  def initialize(variable, value)
+    @variable = variable
+    @value = value
+  end
+
+  def eval
+    Scope.set_variable(@variable, @value.eval)
+  end
+end
+
+class WhileLoop
+  attr_accessor :expr, :block
+
+  def initialize(expr, block)
+    @expr = expr
+    @block = block
+  end
+
+  def eval
+    #TODO
+  end
+end
+
+class IfStatement
+  attr_accessor :expr, :block, :block2
+
+  def initialize(expr, block, block2 = nil)
+    @expr = expr
+    @block = block
+    @block2 = block2
+  end
+
+  def eval
+    if @expr.eval # if expr is true do the block
+      @block.eval
+    elsif !@block2.nil? #if the expr is not true and there is a block2 (else), do the else block then
+      @block2.eval
+    end
+end
+
+class Expression
+  attr_accessor :lhs, :rhs, :operator
+  def initialize(lhs, operator, rhs )
+    @lhs = lhs
+    @operator = operator
+    @rhs = rhs
+  end
+
+  def eval
+    case @operator
+    when '+' then
+      @lhs.eval + @rhs.eval
+    when '-' then
+      @lhs.eval - @rhs.eval
+    when '*' then
+      @lhs.eval * @rhs.eval
+    when '/' then
+      @lhs.eval / @rhs.eval
+    when '<'
+      @lhs.eval < @rhs.eval
+    when '>'
+      @lhs.eval > @rhs.eval
+    when '<='
+      @lhs.eval <= @rhs.eval
+    when '>='
+      @lhs.eval >= @rhs.eval
+    when '=='
+      @lhs.eval == @rhs.eval
+    when '!='
+      @lhs.eval != @rhs.eval
+    else
+      raise ArgumentError, "Invalid comparison operator: #{@operator}"
+    end
+  end
+end
+
+
+class MyVariable
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def eval
+    Scope.find_variable(@name)
+  end
+end
+
+
+class MyInteger
+  attr_accessor :value
+
+  def initialize(value)
+    @value = value.to_i 
+  end
+
+  def eval()
+    return @value
+  end
+end
+
+
+class MyFloat
+  attr_accessor :value
+
+  def initialize(value)
+    @value = value.to_f
+  end
+
+  def eval()
+    return @value
+  end
+end
+
+
+class MyBoolean 
+  attr_accessor :value
+
+  def initialize(value)
+    @value = value.downcase == 'true'
+  end
+  
+  def eval()
+    return @value
+  end
+end
+
+
+class MyCharacter
+  attr_accessor :value
+
+  def initialize(value)
+    @value = value
+  end
+  
+  def eval()
+    if @value.length != 3 or @value[0] != '"' or @value[2] != '"'
+      abort("\nInvalid input!\nPlease enter a single character between two double quotation marks -> example \"c\"" ) ############
+    else
+      return @value[1]
+    end
+  end
+end
+end
